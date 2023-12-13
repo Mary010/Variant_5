c893b1c2f35721e7251b0714816d3d1e453e9fd4
Mary010 <102804838+Mary010@users.noreply.github.com>
2023-12-13 16:35:05 +0300

    Add files via upload

--- 2 lr/var5/Keeper.h
+++ 2 lr/var5/Keeper.h
@@ -14,6 +14,14 @@ public:
     Keeper();
     ~Keeper();
 
+    bool    IsEmpty() const;// Проверка на пустоту
+    void    Push(AEROFLOT obj); // Добавление объекта в контейнер
+    void    LoadInFile(const std::string& filename) ; // Сохранение данных в файл
+    Keeper  LoadFromFile(const std::string& filename); // Загрузка данных из файла
+    void    DisplayAll() const; // Вывод всех объектов, отсортированных по номеру рейса 
+    void    Search() const;   //функция поиска
+    void    Remove();   // Удаление элемента по его индексу
+    void    Edit();//редактирование элемента
 };
 
 
